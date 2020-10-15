1251791ebcd3adb1e786f0b3d5a1fd4eb1f1b396
th3N0m4d <edielton.dantas@hotmail.com>
2020-10-15 19:49:29 +0200

    Create addTask function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,25 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin'
 
-// Start writing Firebase Functions
-// https://firebase.google.com/docs/functions/typescript
+admin.initializeApp()
 
-export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
+export const addTask = functions.https.onRequest(async (req, res) => {
+  const {
+    name,
+    completed,
+    dueDate
+  } = req.body
+
+  try {
+      const result = await admin.firestore().collection('tasks').add({
+          name,
+          completed,
+          dueDate
+      })
+    
+      res.json(`Task created with ID ${result.id} `)      
+  } catch (error) {
+      res.status(500).send(error.message)
+  }
+
 });
