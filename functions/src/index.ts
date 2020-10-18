cfa08b456d101ff638496adbc9b5b8e2fd3720fa
th3N0m4d <edielton.dantas@hotmail.com>
2020-10-18 14:07:13 +0200

    Remove sample cloud function along with its test

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,11 +3,6 @@ import * as admin from 'firebase-admin';
 
 admin.initializeApp();
 
-export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info('Hello logs!', {structuredData: true});
-  response.send('Hello from Firebase!');
-});
-
 export const addTask = functions.https.onRequest(async (req, res) => {
   const {
     name,
